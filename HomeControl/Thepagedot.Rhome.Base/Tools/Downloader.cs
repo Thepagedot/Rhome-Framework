f5d8c92f5be2fe6642d09bd8bf457fdaa4b4bb1e
Robin-Manuel Thiel <robin-manuel.thiel@thepagedot.de>
2015-11-04 01:22:59 +0100

    Fixed Downloader errors

--- HomeControl/Thepagedot.Rhome.Base/Tools/Downloader.cs
+++ HomeControl/Thepagedot.Rhome.Base/Tools/Downloader.cs
@@ -7,33 +7,33 @@ using System.Threading.Tasks;
 
 namespace Thepagedot.Rhome.Base.Tools
 {
-	public static class Downloader
-	{
+    public static class Downloader
+    {
         private static readonly Dictionary<string, CacheFile<string>> Cache = new Dictionary<string, CacheFile<string>>();
 
-	    /// <summary>
-	    /// General download process for API responses
-	    /// </summary>
-	    /// <param name="url">API reference with parameter(s)</param>
-	    /// <param name="cacheTime">Maximum cache age. 'null' if no caching is needed.</param>
-	    /// <returns>Answer string</returns>
-	    public static async Task<string> DownloadWebResponse(string url, TimeSpan? cacheTime = null)
-		{
+        /// <summary>
+        /// General download process for API responses
+        /// </summary>
+        /// <param name="url">API reference with parameter(s)</param>
+        /// <param name="cacheTime">Maximum cache age. 'null' if no caching is needed.</param>
+        /// <returns>Answer string</returns>
+        public static async Task<string> DownloadWebResponse(string url, TimeSpan? cacheTime = null)
+        {
             // Check if cached response is available
-	        if (cacheTime != null)
-	        {
-	            if (Cache.ContainsKey(url))
-	            {
-	                var cacheFile = Cache[url];
-	                if (DateTime.Now.Subtract(cacheFile.TimeStamp) < cacheTime)
-	                {
-	                    return cacheFile.Value;
-	                }
-	            }
-	        }
+            if (cacheTime != null)
+            {
+                if (Cache.ContainsKey(url))
+                {
+                    var cacheFile = Cache[url];
+                    if (DateTime.Now.Subtract(cacheFile.TimeStamp) < cacheTime)
+                    {
+                        return cacheFile.Value;
+                    }
+                }
+            }
 
-			// Create Web Request and avoid caching with header changes
-			var request = (HttpWebRequest)WebRequest.Create(new Uri(url));
+            // Create Web Request and avoid caching with header changes
+            var request = (HttpWebRequest)WebRequest.Create(new Uri(url));
             if (request.Headers == null)
                 request.Headers = new WebHeaderCollection();
 
@@ -47,7 +47,7 @@ namespace Thepagedot.Rhome.Base.Tools
             }
 
             // Download data
-            var response = (HttpWebResponse)await request.GetResponseAsync(new TimeSpan(0, 0, 0, 60));
+            var response = (HttpWebResponse)await request.GetResponseAsync();
             var receiveStream = response.GetResponseStream();
             var readStream = new StreamReader(receiveStream, Encoding.GetEncoding("iso-8859-1"));
             var result = await readStream.ReadToEndAsync();
@@ -55,9 +55,9 @@ namespace Thepagedot.Rhome.Base.Tools
             // Save result to cache
             Cache[url] = new CacheFile<string>(result);
 
-	        return result;
-		}
-	}
+            return result;
+        }
+    }
 
     internal class CacheFile<T>
     {
@@ -72,21 +72,32 @@ namespace Thepagedot.Rhome.Base.Tools
     }
 
     internal static class WebRequestExtensions
-    {
-        internal static Task<WebResponse> GetResponseAsync(this WebRequest request, TimeSpan timeout)
+    {        
+        internal static Task<HttpWebResponse> GetResponseAsync(this HttpWebRequest request)
         {
-            return Task.Factory.StartNew<WebResponse>(() =>
+            var tcs = new TaskCompletionSource<HttpWebResponse>();
+
+            try
             {
-                var t = Task.Factory.FromAsync<WebResponse>(
-                    request.BeginGetResponse,
-                    request.EndGetResponse,
-                    null);
+                request.BeginGetResponse(iar =>
+                {
+                    try
+                    {
+                        var response = (HttpWebResponse)request.EndGetResponse(iar);
+                        tcs.SetResult(response);
+                    }
+                    catch (Exception exc)
+                    {
+                        tcs.SetException(exc);
+                    }
+                }, null);
+            }
+            catch (Exception exc)
+            {
+                tcs.SetException(exc);
+            }
 
-                if (!t.Wait(timeout))
-                    throw new TimeoutException();
-
-                return t.Result;
-            });
+            return tcs.Task;
         }
     }
 }
